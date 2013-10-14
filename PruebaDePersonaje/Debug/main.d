beb7d77e6b25d464d0b21dd989eb2d79f4114350
MendezBruno <brunomendez_sist_info@yahoo.com.ar>
2013-10-14 14:53:02 -0300

    haber que pasa

---
+++ PruebaDePersonaje/Debug/main.d
@@ -0,0 +1,9 @@
+main.d: ../main.c /home/utnso/workspace/commons/commons/config.h \
+ /home/utnso/workspace/commons/commons/collections/dictionary.h \
+ /home/utnso/workspace/commons/commons/collections/node.h
+
+/home/utnso/workspace/commons/commons/config.h:
+
+/home/utnso/workspace/commons/commons/collections/dictionary.h:
+
+/home/utnso/workspace/commons/commons/collections/node.h:
