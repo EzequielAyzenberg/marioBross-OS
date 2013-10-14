0a0ce81e816090c7d2119f8be760379b57989a31
EzequielAyzenberg <ezequiel_ay@hotmail.com>
2013-10-14 17:21:47 -0300

    Agrego libs de cadenas

---
+++ Biblioteca/theGRID/cadenas.h
@@ -0,0 +1,17 @@
+/*
+ * cadenas.h
+ *
+ *  Created on: 14/10/2013
+ *      Author: alumno
+ */
+
+#ifndef CADENAS_H_
+#define CADENAS_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int leecad(char *cad, int n);
+
+#endif /* CADENAS_H_ */
