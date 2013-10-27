df0f8e72ed6d790f6c29c3f53181394ab599fac0
EzequielAyzenberg <ezequiel_ay@hotmail.com>
2013-10-27 13:43:41 -0300

    Empezamos con el orquestador

---
+++ Plataforma/orquestador.c
@@ -0,0 +1,30 @@
+/*
+ * orquestador.c
+ *
+ *  Created on: 27/10/2013
+ *      Author: EzequielAyzenberg
+ */
+
+/* Lista de Eventos tipo Analisis de Sistemas xD
+ *
+ * --DELEGACION DE CONEXIONES--
+ *
+ * RecepcionDeConexiones
+ * ClienteNuevo
+ * ClienteViejo
+ * CrearHiloPlanificador
+ * NivelNuevo
+ * ValidarNiver xD
+ * CrearNuevaTanda
+ *
+ * --KOOPA--
+ *
+ * ChequearKoopa
+ * ActivarKoopa
+ * MatarHilos
+ */
+
+#include "orquestador.h"
+#include "Plataforma.h"
+#include <theGRID/sockets.h>
+
---
+++ Plataforma/orquestador.h
@@ -0,0 +1,13 @@
+/*
+ * orquestador.h
+ *
+ *  Created on: 27/10/2013
+ *      Author: utnso
+ */
+
+#ifndef ORQUESTADOR_H_
+#define ORQUESTADOR_H_
+
+
+
+#endif /* ORQUESTADOR_H_ */
