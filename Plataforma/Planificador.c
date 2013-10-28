c38c5e535a6363f88a1c9e760c332a0a0adbef10
cristianames.92@gmail.com <utnso@utnso.(none)>
2013-10-28 09:56:39 -0300

    Intentando arreglar esto..Nada relevante

--- Plataforma/Planificador.c
+++ Plataforma/Planificador.c
@@ -1,14 +1,15 @@
 
 #include"Planificador.h"
+#define RD_INICIAL 0
 
 void inicializar(info*);
 
 void borrarTanda(info*);
 void crearStruct(nuevo*,player*);
-int leerNovedad(info*,nuevo*,player*);
+int leerNovedad(info*,nuevo**,player*);
 
-void reordenar(player**,player**);
-void cargarAlFinal(player*,player**,player**);
+void reordenar(t_list*ready);
+void cargarAlFinal(player*,t_list*);
 
 void interrupcion(int,short,answer*,global);
 int selectear(answer*,short,fd_set*,int,int,global);
@@ -16,19 +17,16 @@ int selectear(answer*,short,fd_set*,int,int,global);
 int cantleidos=0,RR=0;
 
 void *planificador (void *parametro){
+	puts("\nHola mundo!!--Yo planifico.");
 	info*raiz=(info*)parametro;
-
-//Harcodeo
 	nuevo*actual;
 	actual=raiz->new;
-	stack *recleft;
-	recleft=NULL;
+	t_list *stack,*ready,*sleeps;
+
+	stack=list_create();
+	ready=list_create();
+	sleeps=list_create();
 	player exec;
-	player*readyfte,*readyfin,*sleepfte,*sleepfin;
-	readyfte=NULL;
-	readyfin=NULL;
-	sleepfte=NULL;
-	sleepfin=NULL;
 	fd_set master, copy;
 	int maxfd,rest=0;
 	FD_ZERO(&master);
@@ -37,30 +35,33 @@ void *planificador (void *parametro){
 	maxfd=raiz->nid;
 	global general;
 	general.cabecera=raiz;
-	general.rfte=&readyfte;
-	general.rfin=&readyfin;
-	general.sfte=&sleepfte;
-	general.sfin=&sleepfin;
+	general.ready=ready;
+	general.sleeps=sleeps;
 	general.algo=&RR;
 	general.exe=&exec;
-	general.recur=&recleft;
+	general.recur=stack;
 	general.resto=&rest;
-
+	printf("Nuestro Nivel Se llama: %s\n",raiz->name);
+	puts("\nEnviando Saludos al nivel..");
 	inicializar(raiz);
-
 	player*temp;
 	int estado=1;
 	short respuesta;
-	while (estado!=0){
-		temp=(player*)malloc(sizeof(player));
-		estado=leerNovedad(raiz,actual,temp);
+	while (1){ 		//Solo por ahora lee conexiones!! (estado!=0)
+		player aux;
+		estado=leerNovedad(raiz,&actual,&aux);
 		if(estado!=0){
-			cargarAlFinal(temp,&readyfte,&readyfin);
+			puts("Cargando jugador a la base de datos..");
+			temp=malloc(sizeof(player));
+			*temp=aux;
+			cargarAlFinal(temp,ready);
 			FD_SET(temp->pid,&master);
 			if(temp->pid>maxfd)maxfd=temp->pid;
 			raiz->currplay++;
+			puts("Avisandole al nivel..");
 			sendAnswer(7,0,' ',temp->sym,(short)raiz->nid);
 			copy=master;
+			puts("Selecteando..");
 			respuesta=selectear(NULL,1,&copy,maxfd,raiz->nid,general);
 			switch (respuesta){
 			case 1:puts("--El nivel se ha enterado.--");
@@ -69,21 +70,22 @@ void *planificador (void *parametro){
 				break;
 			}
 		}
+
 	}
-
-
-
-
+puts("El hilo termina ahora!!");
 
 	return 0;
 }
 
 void inicializar(info*raiz){
+	puts("\nPidiendo algoritmo.");
 	sendAnswer(6,0,' ',' ',(short)raiz->nid);
 	sleep(1);
 	answer tempo;
 	recvAnswer(&tempo,(short)raiz->nid);
 	RR=tempo.cont;
+	if(RR==0)puts("Se ha elegido usar el Algoritmo SRDF.");
+	else printf("Se ha elegido usar el Algoritmo Round Robins Q==%d\n",RR);
 }
 
 void borrarTanda(info*raiz){
@@ -98,35 +100,42 @@ void borrarTanda(info*raiz){
 void crearStruct(nuevo*actual,player*temp){
 	temp->pid=actual->pid;
 	temp->sym=actual->sym;
-	temp->data.dist=0;
+	temp->data.dist=RD_INICIAL;
 	temp->data.recsol=' ';
-	temp->stack=NULL;
-	temp->sgte=NULL;
+	temp->stack=list_create();
 }
-int leerNovedad(info*raiz,nuevo*actual,player*temp){
+int leerNovedad(info*raiz,nuevo**actual,player*temp){
 	if (cantleidos==6){
+		puts("Ya van 6..");
 		borrarTanda(raiz);
 		cantleidos=0;
 	}
-	if (actual->pid==0)return 0;
+	if ((*actual)->pid==0)return 0;
 	else{
-		crearStruct(actual,temp);
+		crearStruct(*actual,temp);
+		puts("Se ha coenctado un jugador!!");
+		*actual=(*actual)->sgte;
 	}
 	return 1;
 }
 
-void reordenar(player**lfte,player**lfin){
+bool comparator(void*anterior, void*actual){
+	player *ant,*act;
+	ant=(player*)anterior;
+	act=(player*)actual;
+	if(ant->data.dist>act->data.dist)return true;
+	else return false;
+}
+void reordenar(t_list*ready){
 	if (RR!=0)puts("--Planificación Round Robins => Sin Reordenar--");
 	else{
 		puts("--Planificación SRDF => Reordenando..--");
-		//Tratar de aprender a usar el algoritmo de reordenacion de la catedra.
+		list_sort(ready,comparator);
 	}
 }
-void cargarAlFinal(player*temp,player**lfte,player**lfin){
-	if (*lfin!=NULL)(*lfin)->sgte=temp;
-	*lfin=temp;
-	if (*lfte==NULL)*lfte=*lfin;
-	reordenar(lfte,lfin);
+void cargarAlFinal(player*temp,t_list*ready){
+	list_add(ready, (void*)temp);
+	reordenar(ready);
 }
 
 void interrupcion(int i,short respuesta,answer* aux,global tabla){
@@ -142,7 +151,7 @@ void interrupcion(int i,short respuesta,answer* aux,global tabla){
 	}
 }
 int selectear(answer*tempo,short esperado,fd_set*readfds,int fdmax,int sock,global tabla){
-	answer*aux;
+	answer aux;
 	short respuesta;
 	do{
 		selectGRID(fdmax,readfds);
@@ -153,16 +162,18 @@ int selectear(answer*tempo,short esperado,fd_set*readfds,int fdmax,int sock,glob
 			puts("--ERROR:No se encontro candidato para selectear!!--");
 			exit(1);
 		}else{
-			if (tempo!=NULL)aux=tempo;
-			respuesta=recvAnswer(aux,sock);
+			printf("Se escuchara al socket numero %d\n",i);
+			respuesta=recvAnswer(&aux,i);
 			if (i==sock){
 				if((respuesta==esperado)||(respuesta==-1))return respuesta;
-				else interrupcion(i,respuesta,aux,tabla);
+				else interrupcion(i,respuesta,&aux,tabla);
 			}else{
-				interrupcion(i,respuesta,aux,tabla);
+				interrupcion(i,respuesta,&aux,tabla);
 			}
+			if (tempo!=NULL)*tempo=aux;
 		}
 	}while(1);
 	return -1;
 }
+
 
