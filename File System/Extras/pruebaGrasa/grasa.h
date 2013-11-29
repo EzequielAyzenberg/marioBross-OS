8d17c2d1a2e57df7c4a9b06d7a5f8360d5604c88
mendezbruno <brunomendez_sist_info@yahoo.com.ar>
2013-11-28 21:08:47 -0300

    prueba grasa, FuncionTruncate

---
+++ File System/Extras/pruebaGrasa/grasa.h
@@ -0,0 +1,39 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <xcb/xcb.h>   //libreria chota para usar los int "especiales"
+
+#define GFILEBYTABLE 1024
+#define GFILEBYBLOCK 1
+#define GFILENAMELENGTH 71
+#define GHEADERBLOCKS 1
+#define BLKINDIRECT 1000
+
+typedef uint32_t ptrGBloque;
+
+typedef struct grasa_header_t { // un bloque
+	unsigned char grasa[5];
+	uint32_t version;
+	uint32_t blk_bitmap;
+	uint32_t size_bitmap; // en bloques
+	unsigned char padding[4073];
+} GHeader;
+
+typedef struct grasa_file_t { // un cuarto de bloque (256 bytes)
+	uint8_t state; // 0: borrado, 1: archivo, 2: directorio
+	unsigned char fname[GFILENAMELENGTH];
+	uint32_t parent_dir_block;
+	uint32_t file_size;
+	uint64_t c_date;
+	uint64_t m_date;
+	ptrGBloque blk_indirect[BLKINDIRECT];
+} GFile;
+
